1421ec6657daf4968b8e92cf5769272bbb217d0a
zhou <zhou@zhou>
2018-05-22 15:57:15 +0800

    修改Static

--- src/main/java/statistics/Static.java
+++ src/main/java/statistics/Static.java
@@ -10,15 +10,18 @@ import utility.Global;
 
 public class Static {
 	// 统计所用时间分布
-	public static void staticTotalTime(String fPath) throws Exception{
+	public static void staticTotalTime() throws Exception{
+		String fPath = Global.inputDirectoryPath + "08.if13.else.10.100.testSampleResultFile";
 		BufferedReader br = new BufferedReader(new FileReader(fPath));
+		
 		String line = null;
 		int[] rec = new int[6];
 		int x = 0;
+		int ss = 0;
 		while(null != (line = br.readLine())) {
 			if(line.length()>0 && !line.contains(Global.delimiterPound) && line.charAt(0)>='0' && line.charAt(0)<='9') {
-//				System.out.println(line.split(" ")[12]);
-				x = (Integer.parseInt(line.split(" ")[12]) - 1)/5;
+				System.out.println(line.split(" ")[15]);
+				x = (Integer.parseInt(line.split(" ")[15]) - 1)/5;
 				if(x < 0)	x = 0;
 				else if(x > 5) x = 5;
 				rec[x]++;
@@ -40,18 +43,18 @@ public class Static {
 	// 输出所用总时间
 	public static void printTotal() throws Exception{
 		ArrayList<String> pathList = new ArrayList<>();
-		pathList.add(Global.inputDirectoryPath + "07.if12.else.10.100.testSampleResultFile");
+		pathList.add(Global.inputDirectoryPath + "07.if12.else.now.10.100.testSampleResultFile");
 		pathList.add(Global.inputDirectoryPath + "08.if13.else.10.100.testSampleResultFile");
-		pathList.add(Global.inputDirectoryPath + "09.if13.else2.10.100.testSampleResultFile");
+//		pathList.add(Global.inputDirectoryPath + "09.if13.else2.10.100.testSampleResultFile");
 		
 		ArrayList<BufferedReader> brList = new ArrayList<>();
 		for(String st : pathList) {
 			brList.add(new BufferedReader(new FileReader(st)));
 		}
-		String lineArr[] = new String[3];
+		String lineArr[] = new String[pathList.size()];
 		int count = 1;
 		while(null != (lineArr[0] = brList.get(0).readLine())) {
-			for(int i=1; i<3; i++) {
+			for(int i=1; i<pathList.size(); i++) {
 				lineArr[i] = brList.get(i).readLine();
 			}
 			Boolean sign = Boolean.FALSE;
@@ -72,8 +75,8 @@ public class Static {
 	}
 	
 	public static void main(String[] args) throws Exception{
-		Static.printTotal();
-		
+//		Static.printTotal();
+		Static.staticTotalTime();
 //		String fPath = Global.inputDirectoryPath + "07.if12.else.10.100.testSampleResultFile";
 //		Static.staticTotalTime(fPath);
 		
